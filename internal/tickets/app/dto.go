015240d976212b65e18b39933ec5b64cdcb0c318
hayakawakaki <hayakawakaki@gmail.com>
2026-05-15 15:50:13 +0900

    implement app layer

---
+++ internal/tickets/app/dto.go
@@ -0,0 +1,24 @@
+package app
+
+import (
+	"time"
+
+	"github.com/hayakawakaki/go-racp/internal/tickets/domain"
+)
+
+type TicketListItem struct {
+	LastActivity   time.Time
+	AuthorUsername string
+	Category       string
+	Subject        string
+	Status         domain.Status
+	LastActor      domain.Actor
+	ID             int64
+	MessageCount   int
+	Unread         bool
+}
+
+type TicketDetailDTO struct {
+	Messages []domain.Message
+	Ticket   domain.Ticket
+}
