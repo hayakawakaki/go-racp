05e37c5e6f61edb8809452a54d079d6b3ce8e21f
hayakawakaki <hayakawakaki@gmail.com>
2026-05-29 03:39:25 +0900

    add migrations

---
+++ docker/mariadb/init/01_cp_bridge.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS cp_deposit (
+    id         BIGINT NOT NULL AUTO_INCREMENT,
+    account_id INT    NOT NULL,
+    zeny       BIGINT NOT NULL DEFAULT 0,
+    points     INT    NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS cp_withdraw (
+    id         BIGINT NOT NULL,
+    account_id INT    NOT NULL,
+    zeny       BIGINT NOT NULL DEFAULT 0,
+    points     INT    NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
